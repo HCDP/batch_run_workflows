a2eca6108c7d98ab904ad540f06dec0e18c23c00
jardmclan <mcleanj@hawaii.edu>
2024-04-25 17:46:38 -1000

    allow multiple containers to run in order, add container limits

--- batch_run.py
+++ batch_run.py
@@ -2,8 +2,8 @@
 
 # The data to execute with will be formatted in JSON, like so:
 # {
-#   "container": "ghcr.io/hcdp/preliminary-rainfall-monthly:latest",
-#   "base_env": "/path/to/env_file.env",
+#   "containers": []"ghcr.io/hcdp/preliminary-rainfall-monthly:latest"],
+#   "env": "/path/to/env_file.env",
 #   "dates": [
 #     "2023-01-01",
 #     "2023-01-02",
@@ -22,19 +22,32 @@ import json
 import subprocess
 from datetime import datetime, timedelta
 from time import time_ns
+from math import inf
 
 def parse_date_range(date_range):
     start_date, end_date = date_range.split('_')
     return start_date, end_date
 
-def generate_dates(start_date, end_date):
-    dates = []
-    current_date = datetime.strptime(start_date, '%Y-%m-%d')
-    end_date = datetime.strptime(end_date, '%Y-%m-%d')
+def generate_dates(start_date, end_date, delta, date_format):
+    current_date = datetime.strptime(start_date, date_format)
+    end_date = datetime.strptime(end_date, date_format)
     while current_date <= end_date:
-        dates.append(current_date.strftime('%Y-%m-%d'))
-        current_date += timedelta(days=1)
-    return dates
+        yield current_date.strftime(date_format)
+        current_date += timedelta(**delta)
+
+def run_containers(date, containers, env, dry_run, container_ids, max_containers):
+    for container in containers:
+        print(f'Running container {container} with CUSTOM_DATE={date} and base env file {base_env} at {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+        if not args.dry_run:
+            container_name = f"batch_{date}_{time_ns()}"
+            subprocess.run(['docker', 'run', '-d', f'--name={container_name}', f'--env-file={env}', '-e', f'CUSTOM_DATE={date}', container], check=True, stderr=subprocess.STDOUT)
+            subprocess.run(['docker', 'wait', container_name], check=True, stderr=subprocess.STDOUT)
+
+            container_ids.append(container_name)
+            #remove the oldest container if over the max allowed number of containers have been executed
+            if(len(container_ids) > max_containers):
+                removed_name = container_ids.pop(0)
+                subprocess.run(['docker', 'rm', removed_name], check=True, stderr=subprocess.STDOUT)
 
 def main():
     parser = argparse.ArgumentParser(description='Run a container repeatedly with the given list of CUSTOM_DATE env vars.')
@@ -45,27 +58,25 @@ def main():
     with open(args.data) as f:
         data = json.load(f)
 
-    container = data['container']
-    base_env = data['base_env']
-    dates = data['dates']
+    containers = data['containers']
+    env = data['env']
+    dates = data.get('dates', [])
     date_ranges = data.get('date_ranges', [])
+    delta = data.get("delta", {
+        "days": 1
+    })
+    date_format = data.get("date_format", "%Y-%m-%d")
+    max_containers = data.get("max_stored", inf)
+
+    container_ids = []
 
     for date in dates:
-        print(f'Running container {container} with CUSTOM_DATE={date} and base env file {base_env} at {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
-        if not args.dry_run:
-            container_name = f"batch_ing_{date}_{time_ns()}"
-            subprocess.run(['docker', 'run', '-d', f'--name={container_name}', f'--env-file={base_env}', '-e', f'CUSTOM_DATE={date}', container], check=True, stderr=subprocess.STDOUT)
-            subprocess.run(['docker', 'wait', container_name], check=True, stderr=subprocess.STDOUT)
+        run_containers(date, containers, env, args.dry_run, container_ids, max_containers)
 
     for date_range in date_ranges:
         start_date, end_date = parse_date_range(date_range)
-        dates = generate_dates(start_date, end_date)
-        for date in dates:
-            print(f'Running container {container} with CUSTOM_DATE={date} and base env file {base_env} at {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
-            if not args.dry_run:
-                container_name = f"batch_{date}_{time_ns()}"
-                subprocess.run(['docker', 'run', '-d', f'--name={container_name}', f'--env-file={base_env}', '-e', f'CUSTOM_DATE={date}', container], check=True, stderr=subprocess.STDOUT)
-                subprocess.run(['docker', 'wait', container_name], check=True, stderr=subprocess.STDOUT)
+        for date in generate_dates(start_date, end_date, delta, date_format):
+            run_containers(date, containers, env, args.dry_run, container_ids, max_containers)
 
 if __name__ == '__main__':
     main()
